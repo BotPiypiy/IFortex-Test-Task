3b6e2b1dbac976504d61453c9eded9dc888a537b
Игнат Бельский <ignat.belskiy@gmail.com>
2025-01-12 04:13:28 +0300

    Implement AuthorServive and BookService

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly DateTime FROM_DATE = new DateTime(2015, 1, 1);
+
+        private readonly ApplicationDbContext _dbContext;
+
+ 
+        public AuthorService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<Author> GetAuthor()
+        {
+            return await _dbContext.Authors
+                .OrderByDescending(a => a.Id)
+                .Select(a => new
+                {
+                    Author = a,
+                    LongestTitleLength = a.Books.Max(b => b.Title.Length)
+                })
+                .OrderByDescending(x => x.LongestTitleLength)
+                .ThenBy(x => x.Author.Id)
+                .Select(x => x.Author)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            return await _dbContext.Authors
+            .Select(a => new
+            {
+                Author = a,
+                actualBooks = a.Books.Where(b => b.PublishDate > FROM_DATE)
+            })
+            .Where(x => x.actualBooks.Any() && x.actualBooks.Count() % 2 == 0)
+            .Select(x => x.Author)
+            .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private const string GOAL_NAME = "Red";
+        private readonly DateTime FORM_DATE = new(2012, 5, 25);
+
+
+        private readonly ApplicationDbContext _dbContext;
+
+
+        public BookService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Book> GetBook()
+        {
+            return await _dbContext.Books
+                .OrderByDescending(b => b.Price)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            return await _dbContext.Books
+               .Where(b => b.Title.Contains(GOAL_NAME) && b.PublishDate > FORM_DATE)
+               .ToListAsync();
+        }
+    }
+}
